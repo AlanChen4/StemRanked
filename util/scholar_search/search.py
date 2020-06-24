c8f5e4fc587bf36188f4ce91ced55b93a9f7ed5b
AlanChen4 <avchen4@gmail.com>
2020-06-24 16:16:25 -0400

    added interval searching to avoid ban. code should probably be refactored

--- util/scholar_search/search.py
+++ util/scholar_search/search.py
@@ -38,8 +38,16 @@ def get_profile(name, uni_email):
         print('search attempt failed')
 
 
-def get_faculty(email_domain):
+def get_faculty(email_domain, starting_id=None, limit=None ):
     '''return list of scholar-listed faculty members'''
+    if limit == None:
+        # no limit
+        search_limit = 10000000
+    else:
+        search_limit = limit
+    first_page = True
+    current_profile_count = 0
+
     next_page = "https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors="
     next_page += email_domain
 
@@ -49,9 +57,10 @@ def get_faculty(email_domain):
 
     start_time = time.time()
 
-    get_profile(next_resp.text)
+    if starting_id == None:
+        current_profile_count += get_profile(next_resp.text)
 
-    # continue going to next page, until last page is reached
+    # continue going to next page, until last page is reached, or when limit is hit
     while True:
         # locate the btn to goto next page
         soup = BeautifulSoup(next_resp.text, 'html.parser')
@@ -60,9 +69,21 @@ def get_faculty(email_domain):
 
         # find the link on the button to go the next page 
         next_link = next_btn[0]['onclick'][17:-1]
-        after_author_id = (next_link.split('\\x3d')[-2])[:-10]
-        profile_count = (next_link.split('\\x3d')[-1])
-        next_page = f'https://scholar.google.com/citations?view_op=search_authors&hl=en&mauthors={email_domain}&after_author={after_author_id}&astart={profile_count}'
+        profile_id = (next_link.split('\\x3d')[-1])
+
+        # check if there is a specified starting author
+        if (starting_id == None) or (not first_page):
+            after_author_id = (next_link.split('\\x3d')[-2])[:-10]
+        else:
+            after_author_id = starting_id
+            first_page = False
+
+        # check if limit is reached
+        if current_profile_count >= search_limit:
+            print(f'final [#{current_profile_count}]: {after_author_id}')
+            break
+
+        next_page = f'https://scholar.google.com/citations?view_op=search_authors&hl=en&mauthors={email_domain}&after_author={after_author_id}&astart={profile_id}'
 
         # load the html of the next page
         try:
@@ -80,22 +101,27 @@ def get_faculty(email_domain):
         nav_btns = soup.find_all('button', attrs={'aria-label':True})
         next_btn = list(filter(lambda x: x['aria-label'] == 'Next', nav_btns))
 
-        get_profile(next_resp.text)
+        current_profile_count += get_profile(next_resp.text)
 
         # if there is no KeyError, disabled is True, and last page has been reached
         try:
             disable_check = next_btn[0]['disabled']
             print(f'last page reached: {next_page}')
-            print(f"[FINISHED] ~{profile_count} profile's collected")
+            print(f"[FINISHED] {current_profile_count} profile's collected")
             break
         except KeyError:
             time_record = str(time.time() - start_time)[:4]
-            print(f'[{time_record}] searched through profile #{profile_count}')
+            print(f'[{time_record}] searched through profile #{current_profile_count}')
+            continue
+        except IndexError:
+            print('button could not be found!')
+            print(next_resp.text)
             continue
 
 
 def get_profile(page_html):
     '''writes the profiles onto a .json file'''
+    '''returns the total number of profiles added'''
     profiles = []
     current_profiles = BeautifulSoup(page_html, 'html.parser')
     current_profiles = current_profiles.find_all('h3', class_='gs_ai_name')
@@ -104,11 +130,12 @@ def get_profile(page_html):
             csv_writer = writer(f)
             info = [profile.text, profile.findChildren()[0]['href']]
             csv_writer.writerow(info)
+    return len(current_profiles)
 
 
 def main():
-    # print(get_profile('ketan mayer-patel', 'unc.edu'))
-    get_faculty('duke.edu')
+    get_faculty('duke.edu', starting_id="z_riAPj___8J", limit=500)
+
 
 if __name__ == '__main__':
     main()
