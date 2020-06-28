a9c93c948e8f15be3dc73121cf8299970bec1b78
neel216 <neelr216@gmail.com>
2020-06-28 02:43:25 -0400

    added csv reading and parsing from remote file host

--- src/util/dataReader.js
+++ src/util/dataReader.js
@@ -1,11 +1,33 @@
 import * as Papa from 'papaparse';
 
 function readCSV(subject) {
-    console.log(subject);
-    const file = 'Arizona State University,asu.edu\nAuburn University,auburn.edu\nBinghamton University,binghamton.edu\nBoston College,bc.edu\nBoston University,bu.edu'
-    const parsed = Papa.parse(file);
-    console.log(parsed.data);
-    return parsed.data;
+    const urls = {
+        'test': 'https://srv-file22.gofile.io/download/HtWYEn/publication_information.csv'
+    }
+    const parsed = {};
+
+    console.log('Subject:', subject);
+    const url = urls[subject];
+
+    Papa.parse(url, {
+        download: true,
+        header: true,
+        complete: (results) => {
+            for (let x of results.data) {
+                if (x.Author !== "") { // if the author is not empty --> is a bug on the backend right now
+                    //console.log(x);
+                    parsed[Object.keys(parsed).length] = [x.Author,
+                                                          x.Institution,
+                                                          x.Conference,
+                                                          1 / parseInt(x.NumAuthors, 10),
+                                                          parseInt(x.Year, 10)
+                                                         ];
+                }
+            }
+        }
+    });
+
+    return parsed;
 }
 
 export default readCSV;
