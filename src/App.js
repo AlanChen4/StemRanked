88bcff5c5174f03a7bd7b3f3aa973f33d44a26d8
neel216 <neelr216@gmail.com>
2020-06-28 16:54:53 -0400

    started integrating csv reader and js ranker; added template to csv reader for more subject areas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import readCSV from './util/dataReader';
+import rankings from './util/dataRanker';
 
 function App() {
-  readCSV('test');
+  rankings('test');
   return (
     <div className="App">
       <header className="App-header">
