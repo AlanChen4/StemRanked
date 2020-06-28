abab22c552bed3c3050a452962f524ebd2b69cd1
neel216 <neelr216@gmail.com>
2020-06-28 02:53:05 -0400

    added commenting and TODO

--- src/util/dataReader.js
+++ src/util/dataReader.js
@@ -9,19 +9,15 @@ function readCSV(subject) {
     console.log('Subject:', subject);
     const url = urls[subject];
 
+    // TODO: add error catching in case the file request fails
     Papa.parse(url, {
         download: true,
         header: true,
         complete: (results) => {
             for (let x of results.data) {
                 if (x.Author !== "") { // if the author is not empty --> is a bug on the backend right now
-                    //console.log(x);
-                    parsed[Object.keys(parsed).length] = [x.Author,
-                                                          x.Institution,
-                                                          x.Conference,
-                                                          1 / parseInt(x.NumAuthors, 10),
-                                                          parseInt(x.Year, 10)
-                                                         ];
+                    //console.log(x); // would print each row of the csv
+                    parsed[Object.keys(parsed).length] = [x.Author, x.Institution, x.Conference, 1 / parseInt(x.NumAuthors, 10), parseInt(x.Year, 10)];
                 }
             }
         }
