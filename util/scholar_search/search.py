fd05d24b05f68a5945803fd0307848a04e747d31
AlanChen4 <avchen4@gmail.com>
2020-06-29 14:42:33 -0400

    Added https scheme proxy support, along with important error handling

--- util/scholar_search/search.py
+++ util/scholar_search/search.py
@@ -7,8 +7,8 @@ import requests
 from bs4 import BeautifulSoup
 from csv import writer
 from itertools import cycle
-from requests.exceptions import Timeout
-from scraper_helper import get_proxy_local
+from requests.exceptions import Timeout, ProxyError
+from scraper_helper import get_proxy_local, gen_headers
 
 
 session = requests.Session()
@@ -61,7 +61,8 @@ def get_faculty(email_domain, starting_id=None, limit=None):
     # initial search query
     next_page = "https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors="
     next_page += email_domain
-    next_resp = session.get(url=next_page)
+    referer = 'https://scholar.google.com/citations?view_op=search_authors'
+    next_resp = session.get(url=next_page, headers=gen_headers(referer))
 
     # skip collecting information from base page if there is a beginning author id specified
     if starting_id == None:
@@ -69,55 +70,75 @@ def get_faculty(email_domain, starting_id=None, limit=None):
 
     # prepare proxies
     proxy_path = 'proxies.txt'
-    proxy_list = get_proxy_local(proxy_path, n=30)
+    proxy_list = get_proxy_local(proxy_path, n=20)
     proxy_cycle = cycle(proxy_list)
     proxy_ip = next(proxy_cycle)
 
     # continue going to next page, until last page is reached, or when limit is hit
     while True:
-        # TODO: remove proxy from list if it get's banned
         proxy = {'https': proxy_ip}
+        try:
+            # locate the btn that navigates to next page
+            soup = BeautifulSoup(next_resp.text, 'html.parser')
+            nav_btns = soup.find_all('button', attrs={'aria-label':True})
+            next_btn = list(filter(lambda x: x['aria-label'] == 'Next', nav_btns))
 
-        # locate the btn that navigates to next page
-        soup = BeautifulSoup(next_resp.text, 'html.parser')
-        nav_btns = soup.find_all('button', attrs={'aria-label':True})
-        next_btn = list(filter(lambda x: x['aria-label'] == 'Next', nav_btns))
+            # find the link on the button to go the next page 
+            next_link = next_btn[0]['onclick'][17:-1]
+            profile_id = (next_link.split('\\x3d')[-1])
 
-        # find the link on the button to go the next page 
-        next_link = next_btn[0]['onclick'][17:-1]
-        profile_id = (next_link.split('\\x3d')[-1])
+            # check if there is a specified starting author
+            if (starting_id == None) or (not first_page):
+                after_author_id = (next_link.split('\\x3d')[-2])[:-10]
+            else:
+                after_author_id = starting_id
+                first_page = False
 
-        # check if there is a specified starting author
-        if (starting_id == None) or (not first_page):
-            after_author_id = (next_link.split('\\x3d')[-2])[:-10]
-        else:
-            after_author_id = starting_id
-            first_page = False
-
-        # check if limit is reached
-        if current_profile_count >= search_limit:
-            print(f'final [#{current_profile_count}]: {after_author_id}')
-            break
+            # check if limit is reached
+            if current_profile_count >= search_limit:
+                print(f'final [#{current_profile_count}]: {after_author_id}')
+                break
+        # Index Error occurs when banned
+        except IndexError:
+            pass
 
         # load the html of the next page
         try:
+            # set referer in the header as the previous link requested
+            referer = next_page
+
             next_page = f'https://scholar.google.com/citations?view_op=search_authors&hl=en&mauthors={email_domain}&after_author={after_author_id}&astart={profile_id}'
             next_resp = session.get(url=next_page,
-                    timeout=3,
+                    timeout=5,
+                    headers=gen_headers(referer),
                     proxies=proxy)
         except Timeout:
             print('request timed out')
             continue
+        # remove bad proxy and continue
+        except ProxyError:
+            proxy_list.remove(proxy_ip)
+            proxy_cycle = cycle(proxy_list)
+            proxy_ip = next(proxy_cycle)
+            print(f'[ProxyError] Removed {proxy_ip}')
+            print(f'[Update] {len(proxy_list)} proxies remaining')
+            continue
         else:
             pass
 
-        # locate the "next" button on the scholar page
-        soup = BeautifulSoup(next_resp.text, 'html.parser')
-        nav_btns = soup.find_all('button', attrs={'aria-label':True})
-        next_btn = list(filter(lambda x: x['aria-label'] == 'Next', nav_btns))
+        try:
+            # locate the "next" button on the scholar page
+            soup = BeautifulSoup(next_resp.text, 'html.parser')
+            nav_btns = soup.find_all('button', attrs={'aria-label':True})
+            next_btn = list(filter(lambda x: x['aria-label'] == 'Next', nav_btns))
 
-        # save current profiles onto .csv file and update counter
-        current_profile_count += get_profile(next_resp.text)
+            # save current profiles onto .csv file and update counter
+            current_profile_count += get_profile(next_resp.text)
+        # IndexError occurs when IP Banned
+        except IndexError:
+            pass
+
+        proxy_ip = next(proxy_cycle)
 
         # if there is no KeyError, disabled is True, and last page has been reached
         try:
@@ -126,12 +147,15 @@ def get_faculty(email_domain, starting_id=None, limit=None):
             print(f"[FINISHED] {current_profile_count} profile's collected")
             break
         except KeyError:
-            time_record = str(time.time() - start_time)[:4]
+            time_record = str(time.time() - start_time)
             print(f'[{time_record}] searched through profile #{current_profile_count}')
             continue
         except IndexError:
-            print('button could not be found!')
-            print(next_resp.text)
+            proxy_list.remove(proxy_ip)
+            proxy_cycle = cycle(proxy_list)
+            proxy_ip = next(proxy_cycle)
+            print(f'IP Banned! Removed {proxy_ip}')
+            print(f'[Update] {len(proxy_list)} proxies remaining')
             continue
 
 
