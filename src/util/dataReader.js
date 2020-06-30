1ac63ed40c91fe5db6c4b516383f637e8e681934
neel216 <neelr216@gmail.com>
2020-06-30 04:32:36 -0400

    added async function with promises to allow parsing to complete before program continues

--- src/util/dataReader.js
+++ src/util/dataReader.js
@@ -1,6 +1,6 @@
 import * as Papa from 'papaparse';
 
-function readCSV(subject) {
+async function readCSV(subject) {
     const files = {
         'test': './data/publication_information.csv',
         'namh': './data/generated-author-info.csv',
@@ -12,52 +12,38 @@ function readCSV(subject) {
         'Physics': ''
         // TODO: add corresponding subject area csv path
     };
-    let parsed = {};
-
-    console.log('Subject:', subject);
-    const file = files[subject];
-    let count = 0;
-
-    // TODO: optimize the parser
-    // IDEAS TO TRY:
-    // - Get rid of console.log() statements and figure out a way to time the function
-    // - Enable fastMode in papaparser
-    // - see if parseInt() and parseFloat() is faster than dynamicTyping
-    // - see what Emery did
-    if (subject === 'namh') {
-        Papa.parse(file, {
+    const papaPromise = (filePath) => new Promise((resolve) => {
+        Papa.parse(filePath, {
             download: true,
             header: true,
             dynamicTyping: true,
-            step: (row) => {
-                const info = row.data;
-                parsed[count] = [info.name, info.dept, info.area, info.adjustedcount, info.year];
-                count++;
-            },
-            complete: () => {
-                console.log('And we are done!');
-                console.log(parsed);
+            complete: (results) => {
+                resolve(results);
             }
         });
+    })
+    let parsed = {};
+
+    console.log('Subject:', subject);
+
+    const file = files[subject];
+    let count = 0;
+    const results = await papaPromise(file);
+
+    // TODO: see if parseInt() and parseFloat() is faster than dynamicTyping
+    if (subject === 'namh') {
+        for (let info of results.data) {
+            parsed[count] = [info.name, info.dept, info.area, info.adjustedcount, info.year];
+            count++;
+        }
         
         return parsed;
-        
     }
 
-    Papa.parse(file, {
-        download: true,
-        header: true,
-        dynamicTyping: true,
-        step: (row) => {
-            const info = row.data;
-            parsed[count] = [info.Author, info.Institution, info.Conference, 1 / info.NumAuthors, info.Year];
-            count++;
-        },
-        complete: () => {
-            console.log('And we are done!');
-            console.log(parsed);
-        }
-    });
+    for (let info of results.data) {
+        parsed[count] = [info.Author, info.Institution, info.Conference, 1 / info.NumAuthors, info.Year];
+        count++;
+    }
     
     return parsed;
 }
