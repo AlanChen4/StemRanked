1ae13a017c1dde716355ce61428164c332f48dc0
neel216 <neelr216@gmail.com>
2020-06-30 18:00:36 -0400

    added user input for CSV files and dynamic rendering

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import rankings from './util/dataRanker';
+import InputForm from './InputForm';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <SchoolRankingList subject="namh" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <InputForm />
       </header>
     </div>
-  );
-}
-
-function SchoolRankingList(props) {
-  const [ranks, setRanks] = useState({});
- 
-  useEffect(() => {
-    const fetchData = async (subject) => {
-      const result = await rankings(subject);
-      setRanks(result);
-    };
-    fetchData(props.subject);
-  }, []); // eslint-disable-line
-
-  let school_ranks = [];
-  for (const [key, value] of Object.entries(ranks)) { // eslint-disable-line
-    school_ranks.push(key);
-  }
-  return (
-    <ol>
-      {school_ranks.map(school => <li key={school}>{school}</li>)}
-    </ol>
   );
 }
 
