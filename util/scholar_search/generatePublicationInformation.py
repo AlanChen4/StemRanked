af6f8f93740286a48faa50b617d5f710e82a20f3
oumlahade <oum.lahade@gmail.com>
2020-07-01 04:17:41 -0400

    Update generatePublicationInformation.py
    Added functionality to better parse through extra requests

--- util/scholar_search/generatePublicationInformation.py
+++ util/scholar_search/generatePublicationInformation.py
@@ -1,24 +1,46 @@
-import requests, csv, time
+import requests, csv, re
 from bs4 import BeautifulSoup
 
 session = requests.Session()
 
-def furtherRequest(pub, requestType):
+
+def checkPages(pages):
+    pageCount = 0
+    p_list = pages.split('-')
+    if (len(p_list) !=2):
+        return False
+    try:
+        start = p_list[0]; end = p_list[1]
+        re.sub("\D", "", start); re.sub("\D", "", end)
+        start = int(start); end = int(end)
+        return (end-start >= pageCount)
+    except:
+        return False
+
+def furtherRequest(pub, requestType, beginningString = ''):
     requestFlag = (requestType == 'Check Publication')
     baseUrl = 'https://scholar.google.com'
     additionalUrl = pub.td.a['data-href']
     paper = session.get(url=baseUrl+additionalUrl)
     numAuthors = -1
+    venue = None
     area = None
+
     if (paper.status_code<300 and paper.status_code>=200):
         soup = BeautifulSoup(paper.text, 'html.parser')
         table = list(soup.find_all('div',{'id':'gsc_vcd_table'}))[0]
         if (requestFlag):
-            pass
+            beginningString = beginningString[:round(len(beginningString)/4)]
+            for item in table:                
+                divs = item.find_all('div')
+                if (divs[0].text != 'Scholar articles' and divs[0].text != 'Description'):
+                    if (divs[1].text.find(beginningString) != -1):
+                        venue = divs[1].text
+                
         for item in table:
             if (list(item.find_all('div',{'class':'gsc_vcd_field'}))[0].text == 'Authors'):
                 numAuthors = len(list(list(item.find_all('div',{'class':'gsc_vcd_value'}))[0].text.split(',')))
-        return (area, numAuthors)
+        return (venue, numAuthors)
     else:
         print(f"Paper Request Failed: {baseUrl+additionalUrl}\t\t{paper.status_code}")
         exit()
@@ -26,21 +48,41 @@ def furtherRequest(pub, requestType):
 
 
 def scrapeInfo(pub, auth_name,institution):
+    numAuthors = -1
     pub_info = dict()
     pub_info['Year'] = list(pub.find_all('td',{'class':'gsc_a_y'}))[0].span.text  
     if (pub_info['Year'] == ''):
+        #print('Failure: No Year')
         return False 
     pub_info['Author'] = auth_name 
     pub_info['Institution'] = institution
-    auths = pub.td.div.text
-    numAuthors = -1
+    venue = list(pub.td.find_all('div', {'class':'gs_gray'}))[1].text #contains page numbers, but we cannot remove because the conference may have commas
+    venue = venue[:venue.rfind(',')] #gets rid of the extra year at the end
+    if (venue.find('…') != -1 or venue.find('...') != -1):
+        (venue, numAuthors) = furtherRequest(pub, 'Check Publication', beginningString=venue)
+    else:
+        if (venue.rfind(',') == -1):
+            return False
+        pages = venue[venue.rfind(',')+1:]
+        if (not checkPages(pages)):
+            #print('Failure: Invalid Pages')
+            return False
+        venue = venue[:venue.rfind(',')]
+    
+    print(venue)
+
+
+    auths = pub.td.div.text    
     if (numAuthors != -1):
         pass
     elif (auths.find('...') == -1):
         numAuthors = len(list(auths.split(',')))
     else:
         numAuthors = furtherRequest(pub, 'Authors')[1]
-    pub_info['Adjusted Count']
+    pub_info['Adjusted Count'] = 1/numAuthors
+    #print(pub_info['Adjusted Count'])
+
+
 def check_end(parse): # checks to see if this page is the page after the final urls
     tds = parse.find_all('td')
     try:
@@ -90,4 +132,4 @@ def main(query, institution,csv_file):
     getPages(query, institution)
 
 if __name__ == "__main__":
-    main('https://scholar.google.com/citations?hl=en&user=S4GP-G4AAAAJ','University of North Carolina Chapel Hill', 'publication_information.csv')
+    main('https://scholar.google.com/citations?hl=en&user=ni_ZrQQAAAAJ','Boston University', 'publication_information.csv')
