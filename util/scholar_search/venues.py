1df7c6ea5bad9043604fdb59f5d022c2ee2ae32c
oumlahade <oum.lahade@gmail.com>
2020-07-01 11:41:54 -0400

    Update venues.py
    Added functionality to check if venue is listed in venue_dictionary

--- util/scholar_search/venues.py
+++ util/scholar_search/venues.py
@@ -1,6 +1,7 @@
+import re
 '''
 purpose of this file is to contain dictionaries of all conferences and publications that we will including in our rankings 
-structure = {subject; area; conference_acronyms; conference keywords (for recognition) #conference_name} (https://www.scimagojr.com/journalrank.php?area=2200&category=2210 wieghting off SJR 
+structure = {subject; area; conference_acronyms; conference_name} (https://www.scimagojr.com/journalrank.php?area=2200&category=2210 wieghting off SJR 
 (their ranking system)) (cs conferences from http://www.guide2research.com/topconf/ which are based off h5 index) 
 '''
 
@@ -11,11 +12,11 @@ venue_dictionary = {
         'Image Processing and Computer Vision': {'CVPR':'IEEE Conference on Computer Vision and Pattern Recognition', 'ICCV':'IEEE International Conference on Computer Vision', 'ECCV': 'European Conference on Computer Vision'}, 
         'Machine Learning, Data Mining, & Artificial Intelligence': {'NeurIPS': 'Neural Information Processing Systems', 'ICML':'International Conference on Machine Learning', 'AAAI': 'AAAI Conference on Artificial Intelligence'},
         'Computer Network and Communications': {'CCS':'ACM Symposium on Computer and Communications Security','INFOCOM':'IEEE International Conference on Computer Communications', 'SIGCOMM':'Special Interest Group on Data Communications'},
-        'Human Computer Interaction': {'CHI':'Computer Human Interaction','UIST':'ACM International Conference on Multimedia'},
+        'Human Computer Interaction': {'CHI':'Conference on Human Factors in Computing Systems','UIST':'ACM International Conference on Multimedia'},
         'Software Engineering and Programming Languages': {'ICSE':'International Conference on Software Engineering','PLDI':'ACM SIGPLAN Conference on Programming Language Design and Implementation', 'FSE':'ACM SIGSOFT International Symposium on Foundations of Software Engineering'},
         'Computational Theory and Mathematics': {'STOC':'ACM Symposium on Theory of Computing', 'EUROCRYPT':'International Conference on Theory and Applications of Cryptographic Techniques'},
         'Web, Mobile, and Multimedia Technologies': {'WWW':'International World Wide Web Conferences', 'UbiComp':'ACM Conference on Pervasive and Ubiquitous Computing'},
-        'Computational Linguistics and Speech Processing': {'ACL':'Meeting of the Association for Computational Linguistics', 'EMNLP':'Conference on Empirical Methods in Natural Language Processing', 'NAACL':'Conference of the North American Chapter of the Association for Computational Linguistics: Human Language Technologies'}
+        'Computational Linguistics and Speech Processing': {'ACL':'Meeting of the Association for Computational Linguistics', 'EMNLP':'Conference on Empirical Methods in Natural Language Processing', 'NAACL':'Conference of the North American Chapter of the Association for Computational Linguistics Human Language Technologies'}
     },
 
     'Life Sciences' : {
@@ -74,10 +75,38 @@ venue_dictionary = {
     }
 }
 
-print(venue_dictionary)
 
-def check50(): #checks if 50% of keywords are matched
-    pass
-
-def check100(): #checks if 100% of keyworks are matched
-    pass
+def check(venue, completion = 1): #checks if 50% of keywords are matched
+    nonKeywords = ['of','on','and','the','for','in']
+    print(venue)
+    try:
+        re.sub(r",", "", venue)
+    except:
+        return False
+    ven = venue.lower(); ven = ven.strip(); ven = f" {ven} " #ensure that every string starts and ends with one whitespace
+    for subject in venue_dictionary:
+        for area in venue_dictionary[subject]:
+            try:
+                for acronym in list(venue_dictionary[subject][area].keys()):
+                    count = 0
+                    acr = acronym.lower(); acr = acr.strip()
+                    if (ven.find(f" {acr} ") != -1 or ven.find(f" ({acr}) ") != -1):
+                        if (completion == 1):
+                            return acronym
+                        return True
+                    keywords = list(venue_dictionary[subject][area][acronym].split(' '))
+                    for word in keywords:
+                        word = word.lower(); word = word.strip()
+                        if (word in nonKeywords):
+                            keywords.remove(word)
+                            continue
+                        if (ven.find(word) != -1):
+                            count+=1
+                    if (count>=round(len(keywords)*completion)):
+                        if (completion == 1):
+                            return acronym
+                        return True
+            except:
+                pass
+    return False
+    
