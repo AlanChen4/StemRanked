a5a48327b58cb354c72bd7da1d491a520ce95cd9
oumlahade <oum.lahade@gmail.com>
2020-07-01 04:17:18 -0400

    Update venues.py
    Added cs conferences for Proof of concept

--- util/scholar_search/venues.py
+++ util/scholar_search/venues.py
@@ -1,6 +1,6 @@
 '''
 purpose of this file is to contain dictionaries of all conferences and publications that we will including in our rankings 
-structure = {subject; area; conference_acronyms; conference_names} (https://www.scimagojr.com/journalrank.php?area=2200&category=2210 wieghting off SJR 
+structure = {subject; area; conference_acronyms; conference keywords (for recognition) #conference_name} (https://www.scimagojr.com/journalrank.php?area=2200&category=2210 wieghting off SJR 
 (their ranking system)) (cs conferences from http://www.guide2research.com/topconf/ which are based off h5 index) 
 '''
 
@@ -8,14 +8,14 @@ structure = {subject; area; conference_acronyms; conference_names} (https://www.
 
 venue_dictionary = {
     'Computer Science': { 
-        'Image Processing and Computer Vision': {'CVPR':'IEEE Conference on Computer Vision and Pattern Recognition', 'NeurIPS': 'Neural Information Processing Systems'}, #look at the name in google scholar
-        'Machine Learning, Data Mining, & Artificial Intelligence': None,
-        'Computer Network and Communications': None,
-        'Human Computer Interaction': None,
-        'Software Engineering and Programming Languages': None,
-        'Computational Theory and Mathematics': None,
-        'Web, Mobile, and Multimedia Technologies': None,
-        'Computational Linguistics and Speech Processing': None
+        'Image Processing and Computer Vision': {'CVPR':'IEEE Conference on Computer Vision and Pattern Recognition', 'ICCV':'IEEE International Conference on Computer Vision', 'ECCV': 'European Conference on Computer Vision'}, 
+        'Machine Learning, Data Mining, & Artificial Intelligence': {'NeurIPS': 'Neural Information Processing Systems', 'ICML':'International Conference on Machine Learning', 'AAAI': 'AAAI Conference on Artificial Intelligence'},
+        'Computer Network and Communications': {'CCS':'ACM Symposium on Computer and Communications Security','INFOCOM':'IEEE International Conference on Computer Communications', 'SIGCOMM':'Special Interest Group on Data Communications'},
+        'Human Computer Interaction': {'CHI':'Computer Human Interaction','UIST':'ACM International Conference on Multimedia'},
+        'Software Engineering and Programming Languages': {'ICSE':'International Conference on Software Engineering','PLDI':'ACM SIGPLAN Conference on Programming Language Design and Implementation', 'FSE':'ACM SIGSOFT International Symposium on Foundations of Software Engineering'},
+        'Computational Theory and Mathematics': {'STOC':'ACM Symposium on Theory of Computing', 'EUROCRYPT':'International Conference on Theory and Applications of Cryptographic Techniques'},
+        'Web, Mobile, and Multimedia Technologies': {'WWW':'International World Wide Web Conferences', 'UbiComp':'ACM Conference on Pervasive and Ubiquitous Computing'},
+        'Computational Linguistics and Speech Processing': {'ACL':'Meeting of the Association for Computational Linguistics', 'EMNLP':'Conference on Empirical Methods in Natural Language Processing', 'NAACL':'Conference of the North American Chapter of the Association for Computational Linguistics: Human Language Technologies'}
     },
 
     'Life Sciences' : {
@@ -76,3 +76,8 @@ venue_dictionary = {
 
 print(venue_dictionary)
 
+def check50(): #checks if 50% of keywords are matched
+    pass
+
+def check100(): #checks if 100% of keyworks are matched
+    pass
