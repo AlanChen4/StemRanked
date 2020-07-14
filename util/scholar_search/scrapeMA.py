ecc9db39ec1c6080b328b4997fe56ce872896096
oumlahade <oum.lahade@gmail.com>
2020-07-14 10:20:44 -0400

    added functionality to start from middle if error occurs

--- util/scholar_search/scrapeMA.py
+++ util/scholar_search/scrapeMA.py
@@ -1,4 +1,4 @@
-import microsoftAcademicScraper, csv
+import microsoftAcademicScraper, csv, time
 
 def main(subject):
     institutions = []
@@ -6,12 +6,27 @@ def main(subject):
         reader = csv.reader(f)
         for row in reader:
             institutions.append(row[0])
+
+    last = 'Florida International University'
+    check = True
+    inst = []
+    for i in range(0, len(institutions)):
+        if (check):
+            if (institutions[i] == 'University of North Dakota'):
+                check = False
+        else:
+            inst.append(institutions[i])
+    institutions = inst
+
     
     for i in range(0,len(institutions),2):
+        a = time.time()
         if (i != len(institutions)-1):
             microsoftAcademicScraper.main([institutions[i],institutions[i+1]], subject)
         else:
-            print([institutions[i]],subject)
+            microsoftAcademicScraper.main([institutions[i]],subject)
+        b = time.time()
+        print(f'TIME TAKEN FOR EXECUTION: {(b-a)}')
 
 if __name__ == "__main__":
     main('Computer Science')
