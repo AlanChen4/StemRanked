393255da9c6c8d371ffad0ef00b6ab1115c1e33e
neel216 <neelr216@gmail.com>
2020-07-21 05:11:06 -0400

    replaced sidebar with navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import MainBody from './components/MainBody';
-import { Image, Container, Row, Col, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+import { Image, Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 
 const subjectList = [
   'Computer Science',
@@ -18,52 +18,36 @@ function App() {
 
   const onSubjectChange = (event) => {
     setSelectedSubject(event.currentTarget.value);
-    console.log('Subject selected', event.currentTarget.value);
+    console.log('Subject selected', event);
   }
 
   return (
-    <Container fluid>
-      <Row className="Main">
-        <Col className="Sidebar">
-          <Row className="Title">
-            <Col>
-              <Image className="Title-Logo" src="./images/logo.png" fluid />
-            </Col>
-          </Row>
-          <Row className="Sidebar-Links">
-            <Col>
-              <Row className="Main-Link">
-                <Col xs={4}>
-                  <Image className="Main-Link-Icon" src="./images/subjects-icon.png" />
-                </Col>
-                <Col>
-                  Subjects
-                  <ToggleButtonGroup className="Subject-Link-Container" type="radio" name='subjects' defaultValue={selectedSubject} vertical>
-                    {subjectList.map((subject) => <Row className={selectedSubject === subject ? "Subject-Link-Active" : "Subject-Link-Inactive"} as={ToggleButton} variant="custom" value={subject} onChange={onSubjectChange} block><Col>{subject}</Col></Row>)}
-                  </ToggleButtonGroup>
-                </Col>
-              </Row>
-              <Row className="Main-Link">
-                <Col xs={4}>
-                  <Image className="Main-Link-Icon" src="./images/about-icon.png" />
-                </Col>
-                <Col>
-                  About
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row >
-            <Col className="GitHub-Logo">
-              <a href="https://github.com/AlanChen4/StemRanked" target="_blank" rel="noopener noreferrer" ><Image className="GitHub-Logo-Icon" src="./images/github-logo.png" /></a>
-            </Col>
-          </Row>
-        </Col>
-        <Col>
-          <MainBody subject={selectedSubject} />
-        </Col>
-      </Row>
-    </Container>
+
+    <Navbar className="NavBar" expand="lg" variant="dark">
+      <Navbar.Brand>
+        <Image className="Logo" src="./images/logo.png" />
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav className="mr-auto">
+          <Nav.Link className="SubjectLink">Computer Science</Nav.Link>
+          <Nav.Link className="SubjectLink" onClick={onSubjectChange}>Life Sciences</Nav.Link>
+          <Nav.Link className="SubjectLink">Chemistry</Nav.Link>
+          <Nav.Link className="SubjectLink">Engineering</Nav.Link>
+          <Nav.Link className="SubjectLink">Mathematics</Nav.Link>
+          <Nav.Link className="SubjectLink">Physics</Nav.Link>
+        </Nav>
+        <Nav className="RightSideNavLinks">
+          <Nav.Link className="AboutLink">About</Nav.Link>
+          <a href="https://github.com/AlanChen4/StemRanked" target="_blank" rel="noopener noreferrer" >
+            <Image className="GitHubLogoIcon" src="./images/github-logo.png" />
+          </a>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+
+
+
   );
 }
 
