7248aa207b756464656c6886b7eecd2e59cb817b
oumlahade <oum.lahade@gmail.com>
2020-07-22 14:06:12 -0400

    fixed code to start from beginning

--- util/scholar_search/scrapeMA.py
+++ util/scholar_search/scrapeMA.py
@@ -2,21 +2,21 @@ import microsoftAcademicScraper, csv, time
 
 def main(subject):
     institutions = []
-    with open('./data/institution_domains.csv') as f:
+    with open('util/scholar_search/data/institution_domains.csv') as f:
         reader = csv.reader(f)
         for row in reader:
             institutions.append(row[0])
 
-    last = 'Florida International University'
+    '''last = 'Florida International University'
     check = True
     inst = []
     for i in range(0, len(institutions)):
         if (check):
-            if (institutions[i] == 'University of North Dakota'):
+            if (institutions[i] == last):
                 check = False
         else:
             inst.append(institutions[i])
-    institutions = inst
+    institutions = inst'''
 
     
     for i in range(0,len(institutions),2):
@@ -27,6 +27,7 @@ def main(subject):
             microsoftAcademicScraper.main([institutions[i]],subject)
         b = time.time()
         print(f'TIME TAKEN FOR EXECUTION: {(b-a)}')
+    print(institutions)
 
 if __name__ == "__main__":
-    main('Computer Science')
+    main('Mathematics')
