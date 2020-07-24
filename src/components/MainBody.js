08d9a17aa31db5d9e44d1e66b05a63e5b0abd930
neel216 <neelr216@gmail.com>
2020-07-24 04:14:04 -0400

    render institution rankings

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -3,22 +3,33 @@ import { Row, Col } from 'react-bootstrap';
 import './MainBody.css';
 import { useState, useEffect } from 'react';
 import rankings from '../util/dataRanker';
+import LoadingSpinner from './LoadingSpinner';
+import AuthorRankings from './AuthorRankings';
 
 function MainBody(props) {
-  const [ranks, setRanks] = useState({});
+  const [ranks, setRanks] = useState([]);
   const [authorRanks, setAuthorRanks] = useState({});
+  const [loadingDataStatus, setLoadingDataStatus] = useState(true);
+  const [selectedSchool, setSelectedSchool] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoadingDataStatus(true);
+      setSelectedSchool('loading');
       console.log(props.subject);
       console.log(props.subjectAreas);
       const [result, authorRankings] = await rankings(props.subject, props.subjectAreas, 1970, 2020);
+      let school_ranks = [];
+      for (const [key, value] of Object.entries(result)) { // eslint-disable-line
+        school_ranks.push(key);
+      }
       console.log('Current contents of subAreas:', props.subjectAreas);
       console.log('Current Start Year', 1970);
-      setRanks(result);
+      setRanks(school_ranks);
       setAuthorRanks(authorRankings);
-      //setLoadingDataStatus(false);
-      
+      setSelectedSchool(null);
+      setLoadingDataStatus(false);
+
     };
     fetchData(props.subject);
   }, [props.subject, props.temporary]);
@@ -46,12 +57,29 @@ function MainBody(props) {
             </Row>
             <Row className="InstitutionRankings">
               <Col>
-                Institution Rankings
+                <Row className="TableHeaders">
+                  <Col>
+                    Rank
+                  </Col>
+                  <Col>
+                    Institution
+                  </Col>
+                  <Col>
+                    {/* empty column for spacing ---- NOT SURE IF WE NEED THIS */}
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    {loadingDataStatus ?  <LoadingSpinner /> :
+                      ranks.map((school, i) => <a onClick={() => setSelectedSchool(school)} key={school}><Row className={selectedSchool === school ? "InstitutionSelected" : "Institution"}><Col>{i + 1}</Col><Col>{school}</Col><Col className="Arrow">{selectedSchool === school ? '>' : ''}</Col></Row></a>)
+                    }
+                  </Col>
+                </Row>
               </Col>
             </Row>
           </Col>
           <Col className="AuthorRanks">
-            College Info/Author Rankings
+            <AuthorRankings school={selectedSchool} />
           </Col>
         </Row>
       </Col>
