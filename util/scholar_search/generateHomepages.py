ec6f9dac0642d7ffde9dd52bb37489cb42d25c2b
oumlahade <oum.lahade@gmail.com>
2020-07-25 18:14:04 -0400

    added multithreading to speed up homepage collection, added csv writing functionality

--- util/scholar_search/generateHomepages.py
+++ util/scholar_search/generateHomepages.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import requests, os, csv, time, random
+import requests, os, csv, time, random, threading
 from fake_useragent import UserAgent
 ua = UserAgent()
 print("hello")
@@ -15,18 +15,36 @@ def checkTilda(links, researcher):
             url = link['href'][link['href'].find('=')+1:link['href'].find('&')]
             if (url.find('edu')==-1):
                 continue
+            if (url.find("related")>-1):
+                continue
+            if (url.find('cache')>-1):
+                if (ret != None):
+                    continue
+                try:
+                    url = url[url.find("http"):url.find("+/")]
+                except:
+                    try:
+                        url = url[url.find("http"):url.find("+/")]
+                    except:
+                        continue
+                try:
+                    req = session.get(url).status_code
+                    if (req<200 or req>=300):
+                        continue
+                except:
+                    continue
             if (url.find("~")>-1 and retType!="tilda"): #doesnt have to check for last name
                 ret = url
                 retType = "tilda"
             if (url.find("profile")>-1 and url.lower().find(list(researcher.split(' '))[len(list(researcher.split(' ')))-1].strip().lower())>-1 and retType==None):
                 ret = url
                 retType = "profile"
-            if (url.find("people")>-1 and url.lower().find(list(researcher.split(' '))[len(list(researcher.split(' ')))-1].strip().lower())>-1 and retType!="tilda" and retType!="profile"):
-                ret = url
-                retType = "people"
-            if (url.find("faculty")>-1 and url.lower().find(list(researcher.split(' '))[len(list(researcher.split(' ')))-1].strip().lower())>-1 and retType==None):
+            if (url.find("faculty")>-1 and url.lower().find(list(researcher.split(' '))[len(list(researcher.split(' ')))-1].strip().lower())>-1 and retType!="tilda" and retType!="profile"):
                 ret = url
                 retType = "faculty"
+            if (url.find("people")>-1 and url.lower().find(list(researcher.split(' '))[len(list(researcher.split(' ')))-1].strip().lower())>-1 and retType==None):
+                ret = url
+                retType = "people"
         except:
             pass
     if (ret == None):
@@ -76,20 +94,49 @@ def getFacs(path, faclist):
 					faclist.append((row[0],row[1]))
 					print((row[0],row[1]))
 
+def get(homepages, author, institution):
+    homepages[(author,institution)] = main(author,institution)
+    print(f"({author},{institution}):\t\t{homepages[(author,institution)]}")
+
+def write(directory, filename, homepages):
+    homepageFile = f"{directory}/{filename[:filename.find('.csv')]}_Homepages.csv"
+    with open(homepageFile,'a') as f:
+        writer = csv.writer(f)
+        for auth in homepages.keys():
+            writer.writerow(auth[0],auth[1],homepages[auth])
+
+
 
 def buildFacultyList():
     directory = "public/data"
     for filename in os.listdir(directory):
         faclist = []
         if (filename.endswith("info.csv")):
+            homepageFile = f"{directory}/{filename[:filename.find('.csv')]}_Homepages.csv"
+            if (not os.path.isfile(homepageFile)):
+                with open(homepageFile, 'w') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(['Author', 'Institution','Homepage'])
             getFacs(f"{directory}/{filename}",faclist)
             print(faclist)
             homepages = {}
-            for author in faclist:
+            '''for author in faclist:
                 homepages[(author[0],author[1])] = main(author[0],author[1])
                 print(f"({author[0]},{author[1]}):\t\t{homepages[(author[0],author[1])]}")
-            print(homepages)
-
+            print(homepages)'''
+            numThreads=50
+            for i in range(0,len(faclist),numThreads):
+                threads = []
+                for x in range(numThreads):
+                    try:
+                        threads.append(threading.Thread(target=get,args=(homepages,faclist[(i+x)][0],faclist[(i+x)][1])))
+                    except:
+                        break
+                for thread in threads:
+                    thread.start()
+                for thread in threads:
+                    thread.join()
+            write(directory,filename,homepages)
 
 if __name__ == "__main__":
     #main("Emma Brunskill", "Stanford University")
