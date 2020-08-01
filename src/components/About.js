e6bd8d0410548f34972c340b35993016a137819b
namhlahade <60321173+namhlahade@users.noreply.github.com>
2020-08-01 18:34:08 -0400

    Added the navbar for the about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Image, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { subjectList } from '../constants';
+import './About.css';
 
 function About() {
     return (
-        <div>
-            <Nav className="mr-auto">
-                {subjectList.map((subject) => <Nav.Link href={"/" + subject.replace(' ', '_')} className="SubjectLink" key={subject}>{subject}</Nav.Link>)}
-            </Nav>
+        <div className="Main">
+            <Navbar className="NavBar" expand="xl" variant="dark">
+                <Navbar.Brand>
+                    <Image className="Logo" src="./images/logo.png" />
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav className="Navbar">
+                        {subjectList.map((subject) => <Nav.Link href={"/" + subject.replace(' ', '_')} className="SubjectLink" key={subject}>{subject}</Nav.Link>)}
+                    </Nav>
+                    <Nav className="RightSideNavLinks">
+                        <Nav.Link className="AboutClick">About</Nav.Link>
+                        <a href="https://github.com/AlanChen4/StemRanked" target="_blank" rel="noopener noreferrer" >
+                            <Image className="GitHubLogoIcon" src="./images/github-logo.png" />
+                        </a>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
 
             <p>Picture of Oum Lahade</p>
             <p>Oum Lahade's Bio</p>
