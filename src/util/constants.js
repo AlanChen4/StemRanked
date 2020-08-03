06b0d6c66cd0074f379311523ee9481ab06554dc
namhlahade <60321173+namhlahade@users.noreply.github.com>
2020-08-03 18:27:05 -0400

    temporary commit to check if authoranks methodology works in emery computer science

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,42 +1,42 @@
 export const areaDictionary = {
     'Computer Science': {
-        'Artificial Intelligence': ['aaai', 'aaai conference on artificial intelligence', 'ijcai', 'international joint conference on artificial intelligence'],
-        'Computer Vision': ['cvpr', 'computer vision and pattern recognition', 'eccv', 'european conference on computer vision', 'iccv', 'international conference on computer vision'],
-        'Machine Learning & Data Mining': ['icml', 'international conference on machine learning', 'kdd', 'knowledge discovery and data mining', 'neurips', 'neural information processing systems'],
-        'Natural Language Processing': ['acl', 'meeting of the association for computational linguistics', 'emnlp', 'empirical methods in natural language processing', 'naacl', 'north american chapter of the association for computational linguistics'],
-        'The Web and Information': ['sigir', 'international acm sigir conference on research and development in information retrieval', 'www', 'the web conference'],
-        'Computer Architecture': ['asplos', 'architectural support for programming languages and operating systems', 'isca', 'international symposium on computer architecture', 'micro', 'international symposium on microarchitecture', 'hpca', 'high-performance computer architecture'],
-        'Computer Networks': ['sigcomm', 'acm special interest group on data communication', 'nsdi', 'networked systems design and implementation'],
-        'Computer Security': ['ccs', 'computer and communications security', 's&p', 'ieee symposium on security and privacy', 'usenix', 'usenix security symposium', 'ndss', 'network and distributed system security symposium'],
+        'AI': ['aaai', 'aaai conference on artificial intelligence', 'ijcai', 'international joint conference on artificial intelligence'],
+        'Vision': ['cvpr', 'computer vision and pattern recognition', 'eccv', 'european conference on computer vision', 'iccv', 'international conference on computer vision'],
+        'ML & DM': ['icml', 'international conference on machine learning', 'kdd', 'knowledge discovery and data mining', 'neurips', 'neural information processing systems'],
+        'NLP': ['acl', 'meeting of the association for computational linguistics', 'emnlp', 'empirical methods in natural language processing', 'naacl', 'north american chapter of the association for computational linguistics'],
+        'Web': ['sigir', 'international acm sigir conference on research and development in information retrieval', 'www', 'the web conference'],
+        'Arch': ['asplos', 'architectural support for programming languages and operating systems', 'isca', 'international symposium on computer architecture', 'micro', 'international symposium on microarchitecture', 'hpca', 'high-performance computer architecture'],
+        'Networks': ['sigcomm', 'acm special interest group on data communication', 'nsdi', 'networked systems design and implementation'],
+        'Security': ['ccs', 'computer and communications security', 's&p', 'ieee symposium on security and privacy', 'usenix', 'usenix security symposium', 'ndss', 'network and distributed system security symposium'],
         'Databases': ['sigmod', 'international conference on management of data', 'vldb', 'very large data bases', 'icde', 'international conference on data engineering', 'pods', 'symposium on principles of database systems'],
-        'Design Automation': ['dac', 'design automation conference', 'iccad', 'international conference on computer aided design'],
-        'Embedded and Real-Time Systems': ['emsoft', 'embedded software', 'rtas', 'real time technology and applications symposium', 'rtss', 'real-time systems symposium'],
-        'High Performance Computing': ['hpdc', 'high performance distributed computing', 'ics', 'international conference on supercomputing'],
-        'Mobile Computing': ['mobicom', 'acm/ieee international conference on mobile computing and networking', 'mobisys', 'international conference on mobile systems, applications, and services', 'sensys', 'international conference on embedded networked sensor systems'],
-        'Measurement and Performance Analysis': ['imc', 'internet measurement conference', 'sigmetrics', 'measurement and modeling of computer systems'],
-        'Operating Systems': ['osdi', 'operating systems design and implementation', 'sosp', 'symposium on operating systems principles', 'eurosys', 'european conference on computer systems'],
-        'Programming Languages': ['pldi', 'programming language design and implementation', 'popl', 'symposium on principles of programming languages', 'icfp', 'international conference on functional programming', 'oopsla', 'conference on object-oriented programming systems, languages, and applications'],
-        'Software Engineering': ['fse', 'foundations of software engineering', 'icse', 'international conference on software engineering', 'issta', 'international symposium on software testing and analysis'],
-        'Algorithms & Complexity': ['focs', 'foundations of computer science', 'soda', 'symposium on discrete algorithms', 'stoc', 'symposium on the theory of computing', 'crypto', 'international cryptology conference', 'eurocrypt', 'theory and application of cryptographic techniques'],
-        'Logic and Verification': ['cav', 'computer aided verification', 'lics', 'logic in computer science'],
-        'Comp. bio & bioinformatics': ['ismb', 'intelligent systems in molecular biology', 'recomb', 'research in computational molecular biology'],
-        'Computer Graphics': ['siggraph', 'international conference on computer graphics and interactive techniques', 'siggraph asia', 'siggraph conference and exhibition on computer graphics and interactive techniques in asia'],
-        'Human-Computer Interaction': ['chi', 'human factors in computing systems', 'ubicomp', 'ubiquitous computing', 'uist', 'user interface software and technology'],
+        'DA': ['dac', 'design automation conference', 'iccad', 'international conference on computer aided design'],
+        'Embedded': ['emsoft', 'embedded software', 'rtas', 'real time technology and applications symposium', 'rtss', 'real-time systems symposium'],
+        'HPC': ['hpdc', 'high performance distributed computing', 'ics', 'international conference on supercomputing'],
+        'Mobile': ['mobicom', 'acm/ieee international conference on mobile computing and networking', 'mobisys', 'international conference on mobile systems, applications, and services', 'sensys', 'international conference on embedded networked sensor systems'],
+        'MPA': ['imc', 'internet measurement conference', 'sigmetrics', 'measurement and modeling of computer systems'],
+        'OS': ['osdi', 'operating systems design and implementation', 'sosp', 'symposium on operating systems principles', 'eurosys', 'european conference on computer systems'],
+        'PL': ['pldi', 'programming language design and implementation', 'popl', 'symposium on principles of programming languages', 'icfp', 'international conference on functional programming', 'oopsla', 'conference on object-oriented programming systems, languages, and applications'],
+        'SE': ['fse', 'foundations of software engineering', 'icse', 'international conference on software engineering', 'issta', 'international symposium on software testing and analysis'],
+        'Algorithms': ['focs', 'foundations of computer science', 'soda', 'symposium on discrete algorithms', 'stoc', 'symposium on the theory of computing', 'crypto', 'international cryptology conference', 'eurocrypt', 'theory and application of cryptographic techniques'],
+        'Logic': ['cav', 'computer aided verification', 'lics', 'logic in computer science'],
+        'Bio': ['ismb', 'intelligent systems in molecular biology', 'recomb', 'research in computational molecular biology'],
+        'Graphics': ['siggraph', 'international conference on computer graphics and interactive techniques', 'siggraph asia', 'siggraph conference and exhibition on computer graphics and interactive techniques in asia'],
+        'HCI': ['chi', 'human factors in computing systems', 'ubicomp', 'ubiquitous computing', 'uist', 'user interface software and technology'],
         'Robotics': ['icra', 'international conference on robotics and automation', 'iros', 'intelligent robots and systems', 'rss', 'robotics, science and systems'],
-        'Visualization': ['vis', 'ieee transactions on visualization and computer graphics']
+        'Visual': ['vis', 'ieee transactions on visualization and computer graphics']
     },
 
     'Mathematics': {
-        'Algebra and Number Theory': ['journal of differential geometry','compositio mathematica','forum of mathematics, pi'],
+        'Algebra and Number Theory': ['journal of differential geometry', 'compositio mathematica', 'forum of mathematics, pi'],
         'Analysis': ["annales de l'institut henri poincaré c, analyse non linéaire", "geometric and functional analysis", "archive for rational mechanics and analysis"],
-        'Applied Mathematics':['journal of the american mathematical society',"ieee transactions on pattern analysis and machine intelligence", "communications on pure and applied mathematics"],
-        'Computational Mathematics':["journal of computational physics","journal of computational chemistry"],
-        'Geometry and Topology': ["geometry & topology","geometric and functional analysis", "journal of topology"],
-        'Numerical Analysis': ["acta numerica","r journal","analysis & pde"],
-        'Statistics and Probability': ["journal of statistical software", "journal of machine learning research","journal of the american statistical association", "annals of mathematics", "annals of statistics", "annals of probability"],
-        'Discrete Mathematics and Combinatorics': ["combinatorica","journal of combinatorial theory, series b","journal of combinatorial theory, series a","ieee transactions on information theory"]
+        'Applied Mathematics': ['journal of the american mathematical society', "ieee transactions on pattern analysis and machine intelligence", "communications on pure and applied mathematics"],
+        'Computational Mathematics': ["journal of computational physics", "journal of computational chemistry"],
+        'Geometry and Topology': ["geometry & topology", "geometric and functional analysis", "journal of topology"],
+        'Numerical Analysis': ["acta numerica", "r journal", "analysis & pde"],
+        'Statistics and Probability': ["journal of statistical software", "journal of machine learning research", "journal of the american statistical association", "annals of mathematics", "annals of statistics", "annals of probability"],
+        'Discrete Mathematics and Combinatorics': ["combinatorica", "journal of combinatorial theory, series b", "journal of combinatorial theory, series a", "ieee transactions on information theory"]
     },
-    
+
     'Physics': {
         'Astronomy and Astrophysics': ['websci', 'cidu', 'ims', 'irmmw-thz', 'eqec', 'ursi gass', 'nss', 'icpr', 'cpa', 'phystat', 'qels', 'icops', 'cleo', 'aeroconf', 'sfc', 'nss/mic', 'jicimwicte', 'siggraph', 'e-science', 'rta'],
         'Atomic and Molecular Physics': ['cleo', 'icsd', 'sispad', 'ectc', 'vlsit', 'eqec', 'qels', 'jcis', 'acoft', 'fts', 'pcm', 'icops', 'icmens', 'mines', 'cp', 'cpem', 'ivnc', 'mems', 'iemt'],
@@ -47,7 +47,7 @@ export const areaDictionary = {
     },
 
     'Statistics': {
-        
+
     }
 }
 //Have to change everything to lowercase
