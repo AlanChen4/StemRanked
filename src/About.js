b04f7ecae878bcb2a127c3639d9a2277397fba4f
namhlahade <lahadenamh@gmail.com>
2020-08-08 22:29:09 -0400

    fixed the border issue

--- src/About.js
+++ src/About.js
@@ -48,7 +48,7 @@ function About() {
       </Row>
 
       <h1 className="title">The Rankings</h1>
-      <div className="methodology_border">
+      <div>
         <h6 className="center">The rankings system is based off of an adjusted count found from the number of publications each institution has after the user's selected start date in major conferences. An average count is then created based off of the user's selection of areas with the given formula.</h6>
         <h6 className="center"><Image src="./images/ranking_equation.jpg" /></h6>
       </div>
