d9837a77efa319a06d869657b7beab2f01831b79
neel216 <neelr216@gmail.com>
2020-08-12 17:37:57 -0400

    formatted relative fonts and sizes on About page

--- src/About.js
+++ src/About.js
@@ -23,35 +23,36 @@ function About() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <h1 className="title ">About Us</h1>
-      <Row className="center">
-        <Col>
-          <a href="https://www.linkedin.com/in/namh-lahade-015b33177/" className="center"><Image className="images" src="./images/namh.png" /></a>
-          <h4 className="center2">Namh Lahade</h4>
-          <h6 className="center2">Namh attends Duke University and is currently majoring in Electrical & Computer Engineering and Economics.</h6>
-        </Col>
-        <Col>
-          <a href="https://www.linkedin.com/in/oum-lahade-920b30177/" className="center"><Image className="images" src="./images/oum2.png" /></a>
-          <h4 className="center2">Oum Lahade</h4>
-          <h6 className="center2">Oum attends Duke University and is currently majoring in Electrical & Computer Engineering and Economics.</h6>
-        </Col>
-        <Col>
-          <a href="https://www.linkedin.com/in/neelam-runton-3637291b4/" className="center"><Image className="images" src="./images/neel3.png" /></a>
-          <h4 className="center2">Neel Runton</h4>
-          <h6 className="center2">Neel attends Duke University and is majoring in Electrical & Computer Engineering and Computer Science. At STEM Ranked, he primarily does front-end design and development. In his freetime, he enjoys playing sports like basketball and football.</h6>
-        </Col>
-        <Col>
-          <a href="https://www.linkedin.com/in/alan-chen-aa8994191/" className="center"><Image className="images" src="./images/alan.jpg" /></a>
-          <h4 className="center2">Alan Chen</h4>
-          <h6 className="center2">Alan attends Duke University and is majoring in Computer Science and Economics.</h6>
-        </Col>
-      </Row>
 
       <h1 className="title">The Rankings</h1>
       <div>
-        <h6 className="center">The rankings system is based off of an adjusted count found from the number of publications each institution has after the user's selected start date in major conferences. An average count is then created based off of the user's selection of areas with the given formula.</h6>
-        <h6 className="center"><Image src="./images/ranking_equation.jpg" /></h6>
+        <h6 className="center3">The rankings system is based off of an adjusted count found from the number of publications each institution has after the user's selected start date in major conferences. An average count is then created based off of the user's selection of areas with the given formula.</h6>
+        <h6 className="center"><Image className="images2" src="./images/ranking_equation.jpg" /></h6>
       </div>
+      <br />
+      <h1 className="title ">About Us</h1>
+      <Row className="center">
+        <Col>
+          <a href="https://www.linkedin.com/in/namh-lahade-015b33177/" target="_blank" rel="noopener noreferrer" className="center"><Image className="images1" src="./images/namh.png" /></a>
+          <h4 className="center2">Namh Lahade</h4>
+          <h6 className="center3">Namh attends Duke University and is currently majoring in Electrical & Computer Engineering and Economics.</h6>
+        </Col>
+        <Col>
+          <a href="https://www.linkedin.com/in/oum-lahade-920b30177/" target="_blank" rel="noopener noreferrer" className="center"><Image className="images1" src="./images/oum2.png" /></a>
+          <h4 className="center2">Oum Lahade</h4>
+          <h6 className="center3">Oum attends Duke University and is currently majoring in Electrical & Computer Engineering and Economics.</h6>
+        </Col>
+        <Col>
+          <a href="https://www.linkedin.com/in/neelam-runton-3637291b4/" target="_blank" rel="noopener noreferrer" className="center"><Image className="images1" src="./images/neel3.png" /></a>
+          <h4 className="center2">Neel Runton</h4>
+          <h6 className="center3">Neel attends Duke University and is majoring in Electrical & Computer Engineering and Computer Science.</h6>
+        </Col>
+        <Col>
+          <a href="https://www.linkedin.com/in/alan-chen-aa8994191/" target="_blank" rel="noopener noreferrer" className="center"><Image className="images1" src="./images/alan.jpg" /></a>
+          <h4 className="center2">Alan Chen</h4>
+          <h6 className="center3">Alan attends Duke University and is majoring in Computer Science and Economics.</h6>
+        </Col>
+      </Row>
     </div>
   );
 }
