0797d9a960726addd537db37e399b21a452c9f25
Alan Chen <avchen4@gmail.com>
2020-12-17 21:56:35 -0500

    Add python main file

---
+++ main.py
@@ -0,0 +1,10 @@
+from util.scraper import *
+
+if __name__ == '__main__':
+    clear_db()
+    add_university('duke')
+    add_university('unc')
+    add_university('nc_state')
+
+    add_authors('duke', 'computer_science')
+    add_publications('duke', 'computer_science')
