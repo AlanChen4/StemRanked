704113b8cbbf627816cd4ed221f257a7fa2f2b69
Alan Chen <avchen4@gmail.com>
2021-01-23 16:25:27 -0500

    Replace test code with usable combined function (scrape_field)

--- main.py
+++ main.py
@@ -2,12 +2,4 @@ from util.scraper import *
 
 if __name__ == '__main__':
     clear_db()
-    add_university('duke')
-    add_university('unc')
-    add_university('nc_state')
-
-    add_authors('duke', 'computer science')
-    add_pc_and_id(table_name='duke', uni_name='duke', field='computer science')
-    remove_unrelated_authors(table_name='duke')
-    remove_duplicate_authors(table_name='duke')
-    add_publications('duke', 'computer science')
+    scrape_field('computer science')
